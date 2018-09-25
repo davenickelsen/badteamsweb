587c9438e4aa98861f6e1397203a481221dcbc06
drn417 <drn417@sherwin.com>
2018-09-24 23:18:21 -0400

    Trying to fix CQRS header

--- app.js
+++ app.js
@@ -3,12 +3,18 @@ var express = require("express");
 var app = express();
 app.use(express.static("public"));
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.get("/standings", function(req, res) {
 
 	var callback = function(standings) {
 		console.log("Request for standings complete.");
 		res.setHeader('Content-Type', 'application/json');
-		res.setHeader("Access-Control-Allow-Origin", "*");
+		//res.setHeader("Access-Control-Allow-Origin", "*");
 		res.send(JSON.stringify(standings));
 	};
 	var errorHandler = function(data) {
